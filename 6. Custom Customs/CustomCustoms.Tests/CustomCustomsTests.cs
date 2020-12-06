f25a9c84fe62517360212f224f61def4dedac916
Ben <fitzpattons@gmail.com>
2020-12-06 16:31:19 +0000

    day 6 part 2

--- 6. Custom Customs/CustomCustoms.Tests/CustomCustomsTests.cs
+++ 6. Custom Customs/CustomCustoms.Tests/CustomCustomsTests.cs
@@ -1,6 +1,5 @@
 namespace CustomCustoms.Tests
 {
-    using System;
     using Xunit;
     using System.IO;
     using System.Linq;
@@ -12,7 +11,7 @@ namespace CustomCustoms.Tests
         {
             var input = File.ReadAllLines("../../../test.txt").Append("");
 
-            var expected = new[] 
+            var expected = new[]
             {
                 "abc",
                 "abc",
@@ -21,7 +20,7 @@ namespace CustomCustoms.Tests
                 "b"
             };
 
-            Assert.Equal(expected, Program.SanitiseGroupAnswers(input.ToArray()));
+            Assert.Equal(expected, Program.SanitiseGroupAnswers(input.ToArray(), 1));
         }
 
         [Fact]
@@ -29,7 +28,43 @@ namespace CustomCustoms.Tests
         {
             var input = File.ReadAllLines("../../../test.txt").Append("");
 
-            Assert.Equal(11, Program.CalculateSumOfGroupAnswers(input.ToArray()));
+            Assert.Equal(11, Program.CalculateSumOfGroupAnswers(input.ToArray(), 1));
+        }
+
+        [Fact]
+        public void PartOne_test()
+        {
+            Assert.Equal("defghiknux", Program.PartOne("dxgiekudihnxkgf"));
+        }
+
+        [Fact]
+        public void PartTwo_test()
+        {
+            Assert.Equal("dgikx", Program.PartTwo("dxgiekudihnxkgf", 2));
+        }
+
+        [Fact]
+        public void Sanitise_group_answers_test_part_two()
+        {
+            var input = File.ReadAllLines("../../../test.txt").Append("");
+
+            var expected = new[]
+            {
+                "abc",
+                "a",
+                "a",
+                "b"
+            };
+
+            Assert.Equal(expected, Program.SanitiseGroupAnswers(input.ToArray(), 2));
+        }
+
+        [Fact]
+        public void Calculate_sum_of_group_answers_test_part_two()
+        {
+            var input = File.ReadAllLines("../../../test.txt").Append("");
+
+            Assert.Equal(6, Program.CalculateSumOfGroupAnswers(input.ToArray(), 2));
         }
     }
 }
--- 6. Custom Customs/CustomCustoms/Program.cs
+++ 6. Custom Customs/CustomCustoms/Program.cs
@@ -9,43 +9,73 @@
         static void Main(string[] args)
         {
             var groupAnswers = File.ReadAllLines("./data.txt").Append("");
-            var sum = CalculateSumOfGroupAnswers(groupAnswers.ToArray());
+            var sum = CalculateSumOfGroupAnswers(groupAnswers.ToArray(), int.Parse(args[0]));
 
             Console.WriteLine(sum);
         }
 
-        public static int CalculateSumOfGroupAnswers(string[] input)
+        public static int CalculateSumOfGroupAnswers(string[] input, int puzzlePart)
         {
-            var sanitised = SanitiseGroupAnswers(input);
+            var sanitised = SanitiseGroupAnswers(input, puzzlePart);
 
             return sanitised
                 .Select(g => g.Length)
                 .Aggregate((a, b) => a += b);
         }
 
-        public static string[] SanitiseGroupAnswers(string[] input)
+        public static string[] SanitiseGroupAnswers(string[] input, int puzzlePart)
         {
             var answers = new string[input.Count(i => i == "")];
             var index = 0;
+            var groupMembers = 0;
 
             foreach (var l in input)
             {
                 if (l == "")
                 {
-                    answers[index] = string.Join("",
-                        answers[index]
-                            .ToCharArray()
-                            .OrderBy(i => i)
-                            .Distinct());
+                    var sanitised = puzzlePart == 1
+                        ? PartOne(answers[index])
+                        : PartTwo(answers[index], groupMembers);
 
-                    index++;
+                    if (sanitised != "")
+                    {
+                        answers[index] = sanitised;
+                        index++;
+                    }
+                    else
+                    {
+                        answers[index] = "";
+                    }
+
+                    groupMembers = 0;
+
                     continue;
                 }
 
                 answers[index] += l;
+                groupMembers++;
             }
 
-            return answers;
+            return answers
+                .Where(a => a != null)
+                .ToArray();
+        }
+
+        public static string PartOne(string line)
+        {
+            return string.Join("",
+                    line.ToCharArray()
+                        .OrderBy(i => i)
+                        .Distinct());
+        }
+
+        public static string PartTwo(string line, int groupMembers)
+        {
+            return string.Join("",
+                    line.GroupBy(c => c)
+                        .Where(g => g.Count() == groupMembers)
+                        .Select(g => g.Key)
+                        .OrderBy(i => i));
         }
     }
 }
